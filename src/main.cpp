f5f3e025c5e33ac294179d77b5691541f838dbf6
Ydupc <65360930+CyberDudeJ@users.noreply.github.com>
2023-11-11 22:14:52 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,11 @@ int main() {
   using namespace ftxui;
  
   // The data:
-  std::string first_name;
-  std::string last_name;
+  std::string username;
   std::string password;
-  std::string phoneNumber;
  
   // The basic input components:
-  Component input_first_name = Input(&first_name, "first name");
-  Component input_last_name = Input(&last_name, "last name");
- 
+  Component input_username = Input(&username, "username"); 
   // The password input component:
   InputOption password_option;
   password_option.password = true;
@@ -29,33 +25,20 @@ int main() {
  
   // The phone number input component:
   // We are using `CatchEvent` to filter out non-digit characters.
-  Component input_phone_number = Input(&phoneNumber, "phone number");
-  input_phone_number |= CatchEvent([&](Event event) {
-    return event.is_character() && !std::isdigit(event.character()[0]);
-  });
-  input_phone_number |= CatchEvent([&](Event event) {
-    return event.is_character() && phoneNumber.size() > 10;
-  });
  
   // The component tree:
   auto component = Container::Vertical({
-      input_first_name,
-      input_last_name,
+      input_username,
       input_password,
-      input_phone_number,
-  });
+});
  
   // Tweak how the component tree is rendered:
   auto renderer = Renderer(component, [&] {
     return vbox({
-               hbox(text(" First name : "), input_first_name->Render()),
-               hbox(text(" Last name  : "), input_last_name->Render()),
-               hbox(text(" Password   : "), input_password->Render()),
-               hbox(text(" Phone num  : "), input_phone_number->Render()),
+               hbox(text(" Username: "), input_first_name->Render()),
+               hbox(text(" Password: "), input_password->Render()),
                separator(),
-               text("Hello " + first_name + " " + last_name),
-               text("Your password is " + password),
-               text("Your phone number is " + phoneNumber),
+               text("Hello " + username + "!"),
            }) |
            border;
   });
